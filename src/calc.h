d050fd99282858af0b548cb397e14a3a4fbae629
Victor Milhomem <victormllosada@gmail.com>
2021-05-06 08:44:13 -0300

    calc.h updates

--- src/calc.h
+++ src/calc.h
@@ -1,5 +1,8 @@
-void help();
-void intro();
-void runStd();
-void run_regression();
-void run_prod();
+#ifndef CALC_H
+#define CALC_H
+    void help();
+    void intro();
+    void runStd();
+    void run_regression();
+    void run_prod();
+#endif
